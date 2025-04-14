d793de7217ebcebda41c0b3533cde24699c6adc4
JHJH1289 <128887946+JHJH1289@users.noreply.github.com>
2025-04-14 19:52:24 +0900

    change app.js

--- src/App.js
+++ src/App.js
@@ -4,5 +4,3 @@ import App from './App';  // 🔥 App 컴포넌트 불러오기
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);     // 🔥 App 컴포넌트를 root에 렌더링
-
-export default App;
