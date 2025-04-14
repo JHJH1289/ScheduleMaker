f34aaf086e21232d0079faca9133c6a7bbee1f5b
JHJH1289 <128887946+JHJH1289@users.noreply.github.com>
2025-04-14 20:36:36 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';  // 🔥 App 컴포넌트 불러오기
+import logo from './logo.svg';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);     // 🔥 App 컴포넌트를 root에 렌더링
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
